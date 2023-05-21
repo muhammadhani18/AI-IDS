c619460045425800f85a89c1935be5c10f94f529
mohammadhani18 <mhani18403@gmail.com>
2023-05-21 21:07:20 +0500

    Navbar complete

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -1,79 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react'
 import OnlinePredictionIcon from '@mui/icons-material/OnlinePrediction';
 import HomeIcon from '@mui/icons-material/Home';
 import InfoIcon from '@mui/icons-material/Info';
 import ReportProblemIcon from '@mui/icons-material/ReportProblem';
-import { styled } from '@mui/system';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-
-const SidebarContainer = styled('div')({
-  display: 'flex',
-});
-
-const Sidebar = styled(Drawer)({
-  width: '200px',
-  flexShrink: 0,
-});
-
-const SidebarList = styled(List)({
-  width: '100%',
-});
-
-const SidebarListItem = styled(ListItem)({
-  textDecoration: 'none',
-});
 
 function Nav() {
-  const [open, setOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setOpen(!open);
-  };
-
   return (
-    <SidebarContainer>
-      <Sidebar
-        open={open}
-        onClose={() => setOpen(false)}
-        variant="persistent"
-        anchor="left"
-      >
-        <SidebarList>
-          <SidebarListItem button key="Home">
-            <ListItemIcon>
-              <HomeIcon />
-            </ListItemIcon>
-            <ListItemText primary="Home" />
-          </SidebarListItem>
-          <SidebarListItem button key="About">
-            <ListItemIcon>
-              <InfoIcon />
-            </ListItemIcon>
-            <ListItemText primary="About Project" />
-          </SidebarListItem>
-          <SidebarListItem button key="Reports">
-            <ListItemIcon>
-              <ReportProblemIcon />
-            </ListItemIcon>
-            <ListItemText primary="Reports" />
-          </SidebarListItem>
-        </SidebarList>
-      </Sidebar>
-
-      <div>
-        <nav>
-          <button onClick={toggleSidebar}>
-            <OnlinePredictionIcon sx={{ fontSize: 40 }} />
-          </button>
-        </nav>
-        <h4>Network Analysis</h4>
-      </div>
-    </SidebarContainer>
-  );
+    <>
+    <nav class="navbar navbar-expand navbar-dark sticky-top">
+    <div class="container-fluid">
+        <OnlinePredictionIcon sx={{ fontSize: 40 }}/>
+        <a href="/" class="navbar-brand">
+            <h4>Network Analysis</h4>
+        </a>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav">
+                <a href="/" class="nav-item nav-link active"><HomeIcon/>Home</a>
+                <a href="/" class="nav-item nav-link active"><InfoIcon/>About Project</a>
+                <a href="/" class="nav-item nav-link active" tabindex="-1"><ReportProblemIcon/>Reports</a>
+            </div>
+            
+        </div>
+    </div>
+</nav>
+</>
+  )
 }
 
-export default Nav;
+export default Nav
